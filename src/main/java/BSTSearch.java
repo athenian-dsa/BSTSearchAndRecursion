2d20a8728cfcd478ef5262d4b79e32f0df1ffa61
sthompson-athenian <sthompson@athenian.org>
2024-09-12 09:42:52 -0700

    Initial version of a HW assignment that combines iterative BST Search and some recursion practice

---
+++ src/main/java/BSTSearch.java
@@ -0,0 +1,57 @@
+public class BSTSearch {
+    // The BST keeps track of its root node
+    Node root;
+
+    // At the beginning, there are no nodes
+    public BSTSearch() {
+        this.root = null;
+    }
+
+    // Searches the BST for the given value
+    public boolean search(int val) {
+        // REPLACE WITH YOUR CODE HERE
+        return false;
+    }
+
+    // Factorial Example
+    // The factorial (!) of an integer x is the product
+    // of x and all positive integers less than x
+    // Ex. factorial(5) = 5 * 4 * 3 * 2 * 1 => 120
+    public static int factorial(int x) {
+        // Base case: 1! = 1
+        if (x == 1) {
+            return 1;
+        }
+        // Recursive call: x! = x * (x-1)!
+        return x * factorial(x-1);
+    }
+
+    // Find the n-th fibonacci term.
+    // The first two terms are both equal to 1,
+    // that is fib(1) => 1, and fib(2) => 1
+    // All other terms are equal to the sum of the
+    // previous two terms
+    public static int fib(int n) {
+        // REPLACE WITH YOUR CODE HERE
+        return -1;
+    }
+    // Adds up all the digits in a number
+    // Recall that you can use % 10 to get the last digit
+    public static int sumDigits(int num) {
+        // REPLACE WITH YOUR CODE HERE
+        return -1;
+    }
+}
+
+// Each node will have a data value as well
+// as possibly a left and right child
+class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,12 +1,53 @@
-package test.java;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
     @Test
-    public void testInsertFirst() {
-        assertTrue(false);
+    public void testSearch() {
+        BSTSearch bst = new BSTSearch();
+        Node node10 = new Node(10);
+        Node node2 = new Node(2);
+        Node node5 = new Node(5);
+        Node node7 = new Node(7);
+        Node node3 = new Node(3);
+        Node node6 = new Node(6);
+        Node node11 = new Node(11);
+        Node node13 = new Node(13);
+        Node node12 = new Node(12);
+        bst.root = node7;
+        node7.left = node3;
+        node3.left = node2;
+        node3.right = node6;
+        node6.left = node5;
+        node7.right = node10;
+        node10.right = node12;
+        node12.left = node11;
+        node12.right = node13;
+        assertTrue(bst.search(7));
+        assertTrue(bst.search(6));
+        assertTrue(bst.search(11));
+        assertTrue(bst.search(13));
+        assertFalse(bst.search(1));
+        assertFalse(bst.search(4));
+        assertFalse(bst.search(15));
+        assertFalse(bst.search(100));
+    }
+
+    @Test
+    public void testFib() {
+        assertEquals(1, BSTSearch.fib(1));
+        assertEquals(1, BSTSearch.fib(2));
+        assertEquals(2, BSTSearch.fib(3));
+        assertEquals(3, BSTSearch.fib(4));
+        assertEquals(5, BSTSearch.fib(5));
+        assertEquals(8, BSTSearch.fib(6));
+    }
+
+    @Test
+    public void testSumDigits() {
+        assertEquals(6, BSTSearch.sumDigits(123));
+        assertEquals(5, BSTSearch.sumDigits(5));
+        assertEquals(20, BSTSearch.sumDigits(5537));
     }
 }
